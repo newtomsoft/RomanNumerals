02343da5dc5cd35d6ad5e4a8fd8d99f1e4d26662
Aurélien BOUDOUX <aurelien@boudoux.fr>
2020-09-27 22:47:13 +0200

    proposition refactor

---
+++ RomanNumeralsCore/ConvertHundreds.cs
@@ -0,0 +1,9 @@
+﻿namespace RomanNumeralsCore
+{
+	public class ConvertHundreds : IRomanConverter
+	{
+		private static readonly string[] Hundreds = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+		public string Convert(int number) 
+			=> Hundreds[number / 100 % 10];
+	}
+}
---
+++ RomanNumeralsCore/ConvertTens.cs
@@ -0,0 +1,9 @@
+﻿namespace RomanNumeralsCore
+{
+	public class ConvertTens : IRomanConverter
+	{
+		private static readonly string[] Tens = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+		public string Convert(int number)
+			=> Tens[number / 10 % 10];
+	}
+}
