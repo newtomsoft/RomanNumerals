02b36140d65dd039baeaa77c95f79a9651e2b999
newtomsoft <thomas@newtomsoft.fr>
2020-09-27 12:19:58 +0200

    exemples in main

--- RomanNumeralsConsole/Main.cs
+++ RomanNumeralsConsole/Main.cs
@@ -6,7 +6,11 @@ namespace RomanNumeralsConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = { 1, 2, 4, 5, 9, 10, 25, 45, 50, 57, 100, 500, 1000, 1789, 1515, 1914, 1918, 1939, 1945, 2020, 3999 };
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{number} ->  {RomanNumerals.Convert(number)}");
+            }
         }
     }
 }
